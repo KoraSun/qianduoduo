6967484adb6897cb027ae106d54d40abac87c2e9
kora <bettm9706@gmail.com>
2022-01-22 15:00:11 +0800

    重复三次布局

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        Statistics.vue
+    <div class="nav-wrapper">
+        <div class="content">Statistics.vue</div>
+        
         <Nav />
 
     </div>
@@ -13,5 +14,15 @@
 </script>
 
 <style lang="scss" scoped>
+  .nav-wrapper{
+      border: 1px solid green;
+      display:flex;
+      flex-direction: column;
+      height:100vh;
+  }
+  .content{
+      flex-grow: 1;
+      overflow: auto;
+  }
 
 </style>
