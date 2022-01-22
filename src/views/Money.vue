de37566ab574ebcb3db0d4db13e8549e46f7381d
kora <bettm9706@gmail.com>
2022-01-22 14:42:34 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+             Money.vue
+        </div>
         <Nav />
     </div>
     
@@ -13,5 +15,16 @@ export default {
 </script>
 
 <style scoped>
+  .nav-wrapper{
+      border:1px solid green;
+      display:flex;
+      flex-direction: column;
+      height:100vh
+
+  }
+  .content{
+      overflow: auto;
+      flex-grow:1;
+  }
 
 </style>
