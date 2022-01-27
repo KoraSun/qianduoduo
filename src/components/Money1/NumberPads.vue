e9c37054008ca0906dfddcaa43f71fa2c1b44029
kora <bettm9706@gmail.com>
2022-01-27 14:59:57 +0800

    TS基本完成组件功能

--- src/components/Money1/NumberPads.vue
+++ src/components/Money1/NumberPads.vue
@@ -4,87 +4,65 @@
         </div>
         
         <div class="buttons">
-            <button @click="operate('1')">1</button>
-            <button @click="operate('2')">2</button>
-            <button @click="operate('3')">3</button>
-            <button @click="operate('+')">+</button>
-            <button @click="operate('4')">4</button>
-            <button @click="operate('5')">5</button>
-            <button @click="operate('6')">6</button>
-            <button @click="operate('-')">-</button>
-            <button @click="operate('7')">7</button>
-            <button @click="operate('8')">8</button>
-            <button @click="operate('9')">9</button>
-            <button @click="drop()">←</button>
-            <button @click="operate('.')">.</button>
-            <button @click="operate('0')">0</button>
-            <button @click="equal()">OK</button>
+            <button @click="inputContent">1</button>
+            <button @click="inputContent">2</button>
+            <button @click="inputContent">3</button>
+            <button @click="inputContent">+</button>
+            <button @click="inputContent">4</button>
+            <button @click="inputContent">5</button>
+            <button @click="inputContent">6</button>
+            <button @click="inputContent">-</button>
+            <button @click="inputContent">7</button>
+            <button @click="inputContent">8</button>
+            <button @click="inputContent">9</button>
+            <button @click="remove()">←</button>
+            <button @click="inputContent">.</button>
+            <button @click="inputContent">0</button>
+            <button @click="ok()">OK</button>
             <button @click="cleanAll()">清空</button>
             
         </div>
     </div>
 </template>
 
-<script >
-  export default{
-      name:'NumberPad',
-      data(){
-          return{
-             formula:'',
-             output:'0',
-          }
-      },
-      methods:{
-          operate(element){
-              console.log(element)
-              console.log('operate..')
-              this.formula +=element
-              this.output=this.formula   
-          },
-          drop(){
-              console.log('drop..')
-              this.formula=this.formula.slice(0,-1)
-              this.output=this.formula
-          },
-          equal(){
-              console.log('equal..')
-              console.log(this.formula)
-              this.output=eval(this.formula)
-              this.formula=''
-          },
-          cleanAll(){
-              console.log('clearAll')
-              this.formula=""
-              this.output='0'
-          }
-      }
-
-
-
+<script lang='ts'>
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+    @Component
+    export default class NumberPad extends Vue{
+        output:string ='0';
+        formula:string='0';
+        inputContent(event:MouseEvent){
+            const button= (event.target as HTMLButtonElement);
+            const input = button.textContent !;
+            if(this.output.length=== 16){return;}
+            if(this.output === '0'){
+              if('0123456789'.indexOf(input)>=0){
+                  this.output = input;
+              }else{
+                this.output += input;
+              }
+              return;     
+            }if(this.output.indexOf('.')>=0 && input==='.'){return;}
+            this.output+=input;
+            this.formula=this.output
+            
+        }
+        remove(){
+            if(this.output.length===1){
+                this.output='0';
+            }else{
+                this.output=this.output.slice(0,-1)
+            }
+        }
+        cleanAll(){
+            this.output='0'
+        }
+        ok(){
+            this.output=(eval(this.formula)).toString()
+            
+        }
   }
-    
-    // import Vue from 'vue';
-    // import {Component} from 'vue-property-decorator';
-    // @Component
-    // export default class NumberPad extends Vue{
-    //     output:string ='0';
-    //     inputContent(event:MouseEvent){
-    //         const button= (event.target as HTMLButtonElement);
-    //         const input = button.textContent!;
-    //         if(this.output === '0'){
-    //             if(input === '0'){
-    //                 return;
-    //             }else if('123456789'.indexOf(input)>=0){
-    //                 this.output = input;
-    //                 return;
-    //             }
-    //         }else{
-    //             this.output += button.textContent
-    //         }
-                
-    //     };
-       
-    // }
 </script>
 
 <style lang="scss" scoped>
