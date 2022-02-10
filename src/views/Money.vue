577e10d0e3d92997e4068627370b894b542ba9b6
kora <bettm9706@gmail.com>
2022-02-10 17:32:36 +0800

    封装一个model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,6 @@
 
 <template>
     <Layout class-prefix="layout"> 
-        {{recordList}}
         <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
         <Notes @update:value="onUpdateNotes"/>
         <Tags :data-source.sync="tags" @update:value="onUpdateTags"/> 
@@ -16,24 +15,17 @@ import Tags from "@/components/Money1/Tags.vue";
 import Notes from "@/components/Money1/Notes.vue";
 import Types from "@/components/Money1/Types.vue";
 import NumberPad from "@/components/Money1/NumberPads.vue";
+import model from "@/model"
 
-const recordList:Record[]=JSON.parse(window.localStorage.getItem('recordList') ||'[]');
-
-type Record={
-    tags:string[];
-    notes:string;
-    type:string;
-    amount:number;
-    createdAt?:Date;
-}
+const recordList= model.fetch();
 
 @Component({
      components: {Tags,Notes,Types,NumberPad},
 })
 export default class Money extends Vue{
   tags=['三餐','零食','衣服','交通','学习','住房','美妆','医疗','运动',];
-  recordList:Record[]=recordList;
-  record: Record={
+  recordList:RecordItem[]=recordList;
+  record: RecordItem={
       tags:[],notes:'',type:'-',amount:0,
   };
   onUpdateTags(value:string[]){
@@ -47,13 +39,13 @@ export default class Money extends Vue{
       this.record.amount=parseFloat(value)
   }
   saveRecord(){
-      const record2:Record =JSON.parse(JSON.stringify(this.record));
+      const record2:RecordItem =model.clone(this.record);
       record2.createdAt= new Date();
       this.recordList.push(record2);
   }
   @Watch('recordList')
   onRecordListChange(){
-      window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+     model.save(this.recordList)
   }
 
  
